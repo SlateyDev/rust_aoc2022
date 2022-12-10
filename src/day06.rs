91e0e94faefd05a019d5e4cb74d8130d0e5357c8
SlateyTV <slatey79@gmail.com>
2022-12-11 00:31:37 +1100

    Translated Day 6

--- src/day06.rs
+++ src/day06.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use crate::common_funcs::read_lines;
 
 pub fn run() {
@@ -7,9 +8,15 @@ pub fn run() {
 
 fn part1() {
     if let Ok(lines) = read_lines(format!("input-{}.txt", file!().replace("src\\", "").replace(".rs", ""))) {
-        let mut answer : u32 = 0;
-        for line in lines {
-            if let Ok(line_data) = line {
+        let mut answer: u32 = 0;
+        let marker_size = 4;
+        let chars = lines.into_iter().map(|n| n.unwrap()).collect::<Vec<String>>()[0].chars().collect::<Vec<char>>();
+        for char_index in 0..chars.len() - marker_size {
+            let slice = &chars[char_index..char_index + marker_size];
+            let hs = HashSet::<char>::from_iter(slice.iter().cloned());
+            if hs.len() == marker_size {
+                answer = (char_index + marker_size) as u32;  //Add the 4 characters since our index doesn't take that into account
+                break;
             }
         }
         println!("Day {} part 1: {}", file!().replace("src\\day", "").replace(".rs", ""), answer);
@@ -18,9 +25,15 @@ fn part1() {
 
 fn part2() {
     if let Ok(lines) = read_lines(format!("input-{}.txt", file!().replace("src\\", "").replace(".rs", ""))) {
-        let mut answer : u32 = 0;
-        for line in lines {
-            if let Ok(line_data) = line {
+        let mut answer: u32 = 0;
+        let marker_size = 14;
+        let chars = lines.into_iter().map(|n| n.unwrap()).collect::<Vec<String>>()[0].chars().collect::<Vec<char>>();
+        for char_index in 0..chars.len() - marker_size {
+            let slice = &chars[char_index..char_index + marker_size];
+            let hs = HashSet::<char>::from_iter(slice.iter().cloned());
+            if hs.len() == marker_size {
+                answer = (char_index + marker_size) as u32;  //Add the 4 characters since our index doesn't take that into account
+                break;
             }
         }
         println!("Day {} part 2: {}", file!().replace("src\\day", "").replace(".rs", ""), answer);
